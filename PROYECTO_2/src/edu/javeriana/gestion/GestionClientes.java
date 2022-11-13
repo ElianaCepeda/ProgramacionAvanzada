c49a5d254889613ba18ee59551546c79e6c33575
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2022-11-13 10:14:30 -0500

    Adicionando reintentos a la seleccion del codigo del cliente y ajustes menores al modificar

--- PROYECTO_2/src/edu/javeriana/gestion/GestionClientes.java
+++ PROYECTO_2/src/edu/javeriana/gestion/GestionClientes.java
@@ -11,7 +11,7 @@ public class GestionClientes {
 
     public List<Cliente> CrearLista( )
     {
-        int ia;
+        int ia = -1;
         long cod, TID, telefono;
         String nom, aph, direcc;
 
@@ -21,7 +21,11 @@ public class GestionClientes {
 
         System.out.println("BIENVENIDO DE NUEVO...DIGANOS ¿Cuántos clientes hay en galería?");
         ia = scanner.nextInt();
-
+        while(ia < 1 )
+        {
+            System.out.println("El numero de clientes no puede ser menor que 1 ... DIGANOS ¿Cuántos clientes hay en galería?");
+            ia = scanner.nextInt();
+        }
         for(int y=0; y<ia; y++)
         {
             System.out.println(" Vamos a llenar la lista... Escriba los Datos de TODOS LOS CLIENTES");
@@ -37,9 +41,9 @@ public class GestionClientes {
             direcc = scanner.next();
             System.out.println("Telefonos");
             telefono = scanner.nextLong();
-
-
-            Cliente numerosos = new Cliente(cod, TID, nom, aph, direcc, telefono);
+            
+            
+            Cliente numerosos = new Cliente(cod, TID, nom, aph, direcc, telefono,true, null);
             lista.add(numerosos);
 
         }
@@ -47,24 +51,29 @@ public class GestionClientes {
       return lista;
     }
 
-    public void InsertarnuevoCliente( List<Cliente> actuales)
+    public void InsertarNuevoCliente( List<Cliente> actuales)
     {
         long cod, TID, telefono;
         String nom, aph, direcc;
-        boolean hy=false;
+        boolean hy=true;
 
         Scanner scanner = new Scanner (System.in);
 
         Cliente ab = null;
-        GestionObras ff = null;
         System.out.println(" Vamos a Agregar una nuevo cliente a la lista... Escriba sus datos");
         System.out.println(" Codigo del Cliente: ");
         cod = scanner.nextLong();
-
-        for(int u=0; u<actuales.size(); u++)
+        
+        while(hy==true)
         {
-            if(cod== actuales.get(u).getCodigoCliente())
-                hy=true;
+        	hy=false;
+	        for(int u=0; u<actuales.size(); u++)
+	        {
+	            if(cod== actuales.get(u).getCodigoCliente())
+	                hy=true;
+	        }
+	        if(hy)
+	            System.out.println("-----El cliente no existe, por favor digite nuevamente el codigo de cliente: -----");
         }
         if(hy==false)
         {
@@ -81,11 +90,9 @@ public class GestionClientes {
             System.out.println("Telefonos");
             telefono = scanner.nextLong();
 
-            ab = new Cliente(cod, TID, nom, aph, direcc, telefono);
+            ab = new Cliente(cod, TID, nom, aph, direcc, telefono, true, null);
             actuales.add(ab);
-        }else if(hy==true)
-                System.out.println("-----La obra ya existe, no se puede insertar la obra-----");
-
+        }
 
     }
     public void Modificar(List<Cliente> mod)
@@ -95,67 +102,68 @@ public class GestionClientes {
         String nom, aph, direcc;
         boolean jj = false;
         boolean hh = false;
+        Cliente clienteEncontrado = null;
 
         Scanner scanner = new Scanner (System.in);
-
-        System.out.println("¿Cuál Cliente? ::: (Diga su codigo)");
-        i = scanner.nextLong();
-        for(yu =0; yu< mod.size(); yu++)
+        while(jj==false)
         {
-            if(i==mod.get(yu).getCodigoCliente())
-                jj=true;
+        	jj=false;
+	        System.out.println("¿Cuál Cliente? ::: (Diga su codigo)");
+	        i = scanner.nextLong();
+	        for(yu =0; yu< mod.size(); yu++)
+	        {
+	            if(i==mod.get(yu).getCodigoCliente())
+	            {
+	                jj=true;
+	                clienteEncontrado = mod.get(yu);
+	            }
+	        }
+	        if(jj==false)
+	            System.out.println("No se pudo modificar nada pues el cliente con codigo " + i +"no existe" );
         }
-
         if(jj==true) {
             System.out.println("¿Qué quiere modificar? ::: (1. Codigo del Cliente 2. cedula 3. Nombre 4. Apellidos 5. Dirección de Entrega 6. Telefono)");
             ans = scanner.nextInt();
-            for (int u = 0; u < mod.toArray().length; u++) {
-                if (u == yu) {
-                    if (ans == 1) {
-                        System.out.println("Escriba el nuevo valor");
-                        cod = scanner.nextLong();
-                        for(int g =0; g< mod.size(); g++)
-                        {
-                            if(cod==mod.get(g).getCodigoCliente())
-                                hh=true;
-                        }
-                        if(hh==true)
-                            mod.get(u).setCodigoCliente(cod);
-                        else if(hh==false)
-                            System.out.println("El codigo ya existe, por ello no se pudo modificar el codigo" );
-
-                    }
-                    if (ans == 2) {
-                        System.out.println("Escriba el nuevo valor");
-                        TID = scanner.nextLong();
-                        mod.get(u).setCedula(TID);
-                    }
-                    if (ans == 3) {
-                        System.out.println("Escriba el nuevo Nombre");
-                        nom = scanner.next();
-                        mod.get(u).setNombre(nom);
-                    }
-                    if (ans == 4) {
-                        System.out.println("Escriba los Nuevos Apellidos");
-                        aph = scanner.next();
-                        mod.get(u).setApellidos(aph);
-                    }
-                    if (ans == 5) {
-                        System.out.println("Escriba el nuevo valor");
-                        direcc = scanner.next();
-                        mod.get(u).setDireccionEntrega(direcc);
-                    }
-                    if (ans == 6) {
-                        System.out.println("Escriba el nuevo valor");
-                        telefono = scanner.nextLong();
-                        mod.get(u).setTelefono(telefono);
-                    }
-
+            if (ans == 1)
+            {
+                System.out.println("Escriba el nuevo valor");
+                cod = scanner.nextLong();
+                for(int g =0; g< mod.size(); g++)
+                {
+                    if(cod==mod.get(g).getCodigoCliente())
+                        hh=true;
                 }
+                if(hh==true)
+                    clienteEncontrado.setCodigoCliente(cod);
+                else if(hh==false)
+                    System.out.println("El codigo ya existe, por ello no se pudo modificar el codigo" );
             }
-        } else if(jj==false)
-            System.out.println("No se pudo modificar nada pues el cliente con codigo " + i +"no existe" );
-
+            if (ans == 2) {
+                System.out.println("Escriba el nuevo valor");
+                TID = scanner.nextLong();
+                clienteEncontrado.setCedula(TID);
+            }
+            if (ans == 3) {
+                System.out.println("Escriba el nuevo Nombre");
+                nom = scanner.next();
+                clienteEncontrado.setNombre(nom);
+            }
+            if (ans == 4) {
+                System.out.println("Escriba los Nuevos Apellidos");
+                aph = scanner.next();
+                clienteEncontrado.setApellidos(aph);
+            }
+            if (ans == 5) {
+                System.out.println("Escriba el nuevo valor");
+                direcc = scanner.next();
+                clienteEncontrado.setDireccionEntrega(direcc);
+            }
+            if (ans == 6) {
+                System.out.println("Escriba el nuevo valor");
+                telefono = scanner.nextLong();
+                clienteEncontrado.setTelefono(telefono);
+            }
+        }
     }
     public void Eliminar(List<Cliente> gg)
     {
@@ -165,20 +173,22 @@ public class GestionClientes {
 
         Scanner scanner = new Scanner (System.in);
 
-        System.out.println("¿Cuál Cliente va a eliminar? ::: (Diga su posición)");
-        i = scanner.nextLong();
-
-        for(yu =0; yu< gg.size(); yu++)
+        while(jj==false)
         {
-            if(i==gg.get(yu).getCodigoCliente())
-                jj=true;
+        	jj=false;
+	        System.out.println("¿Cuál Cliente va a eliminar? ::: (Diga su codigo)");
+	        i = scanner.nextLong();
+	        for(yu =0; yu< gg.size(); yu++)
+	        {
+	            if(i==gg.get(yu).getCodigoCliente())
+	            {
+	                jj=true;
+	                gg.remove(yu);
+	            }
+	        }
+	        if(jj==false)
+	            System.out.println("No se pudo eliminar nada pues el cliente con codigo " + i +"no existe" );
         }
-        if(jj==true)
-        {
-            if(gg.get(yu).getEstado()==true)
-                gg.remove(yu);
-        } else if(jj==false)
-            System.out.println("No se pudo eliminar nada pues el cliente con codigo " + i +"no existe" );
 
     }
 
