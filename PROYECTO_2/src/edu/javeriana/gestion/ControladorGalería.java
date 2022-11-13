fb1ca6c9a13c5273fcc68113c69a26de3b79f8ad
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2022-11-13 10:50:24 -0500

    Estandarizacion de nombres de las funciones

--- PROYECTO_2/src/edu/javeriana/gestion/ControladorGalería.java
+++ PROYECTO_2/src/edu/javeriana/gestion/ControladorGalería.java
@@ -12,32 +12,17 @@ public class ControladorGalería {
 
 	private List<Obra> listaObras = new LinkedList<>();
 	private List<Artista> listaArtistias = new LinkedList<>();
-	public GestionObras gestioObras = new GestionObras();
+	private GestionObras gestionObras = new GestionObras();
 	private List<Cliente> listaClientes = new LinkedList<>();
-	public GestionClientes gestionCliente = new GestionClientes();
+	private GestionClientes gestionCliente = new GestionClientes();
 
 	public ControladorGalería() {
-		this.listaObras = gestioObras.CrearLista();
-		gestioObras.CrearArtista(this.listaArtistias);
+		this.listaObras = gestionObras.CrearLista();
+		gestionObras.CrearArtista(this.listaArtistias);
 		this.listaClientes = gestionCliente.CrearLista();
 	}
 
-	public void ListaObras() throws IOException {
-		try (Scanner scanner = new Scanner(System.in)) {
-			boolean ans;
-			System.out.println("¿Va a modificar alguna obra?");
-			ans = scanner.nextBoolean();
-			if (ans == true)
-				gestioObras.Modificar(this.listaObras);
-			System.out.println("¿Va a eliminar alguna obra?");
-			ans = scanner.nextBoolean();
-			if (ans == true)
-				gestioObras.Eliminar(this.listaObras);
-		}
-
-	}
-
-	public void verlistaObrasDisponibles() {
+	public void VerListaObrasDisponibles() {
 		for (int y = 0; y < listaObras.size(); y++) {
 			if (listaObras.get(y).estado == true) {
 				System.out.println("-------------------");
@@ -52,10 +37,23 @@ public class ControladorGalería {
 		}
 	}
 
-	public void BuscarObra() throws IOException {
+	public void VerListaClientes() {
+	
+		for (int p = 0; p < listaClientes.size(); p++) {
+			System.out.println("-------------------");
+			System.out.println("Cliente " + (p + 1) + ":");
+			System.out.println("Nombre: " + listaClientes.get(p).getNombre());
+			System.out.println("Apellido : " + listaClientes.get(p).getApellidos());
+			System.out.println("Dirección : " + listaClientes.get(p).getDireccionEntrega());
+			System.out.println("Telefono : " + listaClientes.get(p).getTelefono());
+		}
+	}
+
+	public Obra BuscarObra() throws IOException {
 		int dia, mes, año;
 		String ans, busca;
 		LocalDate uy;
+		Obra obraEncontrada = null;
 
 		try (Scanner scanner = new Scanner(System.in)) {
 			System.out.println("HOLA...POR DONDE QUIERES BUSCAR LA OBRA(T para titulo, A para artista o I por fecha)");
@@ -64,28 +62,29 @@ public class ControladorGalería {
 			if (ans.equalsIgnoreCase("T")) {
 				System.out.println("Escriba el titulo que quiera buscar");
 				busca = scanner.next();
-				gestioObras.BuscaporTitulo(listaObras, busca);
+				obraEncontrada= gestionObras.BuscaporTitulo(listaObras, busca);
 			} else if (ans.equalsIgnoreCase("A")) {
 				System.out.println("Escriba el Artista que quiera buscar");
 				busca = scanner.next();
-				gestioObras.BuscaporArtista(listaObras, busca);
+				obraEncontrada = gestionObras.BuscaporArtista(listaObras, busca);
 			} else if (ans.equalsIgnoreCase("I")) {
 				System.out.println("Escriba la fecha que quiera buscar...(1. año - 2.mes - 3.dia)");
 				año = scanner.nextInt();
 				mes = scanner.nextInt();
 				dia = scanner.nextInt();
 				uy = LocalDate.of(año, mes, dia);
-				gestioObras.BuscaporFecha(listaObras, uy);
+				obraEncontrada = gestionObras.BuscaporFecha(listaObras, uy);
 			} else
 				System.out.println("LA OPCIÓN NO EXISTE");
 		}
+		return obraEncontrada;
 	}
 
 	public void AgregarObra() {
-		gestioObras.InsertarnuevaObra(listaObras, listaArtistias);
+		gestionObras.InsertarnuevaObra(listaObras, listaArtistias);
 	}
 
-	public void ListaClientes() throws IOException {
+	public void MenuListaClientes() throws IOException {
 		try (Scanner scanner = new Scanner(System.in)) {
 			boolean ans1;
 
@@ -101,16 +100,19 @@ public class ControladorGalería {
 
 	}
 
-	public void verListaClientes() {
-
-		for (int p = 0; p < listaClientes.size(); p++) {
-			System.out.println("-------------------");
-			System.out.println("Cliente " + (p + 1) + ":");
-			System.out.println("Nombre: " + listaClientes.get(p).getNombre());
-			System.out.println("Apellido : " + listaClientes.get(p).getApellidos());
-			System.out.println("Dirección : " + listaClientes.get(p).getDireccionEntrega());
-			System.out.println("Telefono : " + listaClientes.get(p).getTelefono());
+	public void MenuListaObras() throws IOException {
+		try (Scanner scanner = new Scanner(System.in)) {
+			boolean ans;
+			System.out.println("¿Va a modificar alguna obra?");
+			ans = scanner.nextBoolean();
+			if (ans == true)
+				gestionObras.Modificar(this.listaObras);
+			System.out.println("¿Va a eliminar alguna obra?");
+			ans = scanner.nextBoolean();
+			if (ans == true)
+				gestionObras.Eliminar(this.listaObras);
 		}
+	
 	}
 
 }
